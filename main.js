60cdcfd263024c5cfb139bd9a1d209f2562d9f08
Lazar Manasijevic <lazar0169@gmail.com>
2017-08-07 23:09:19 +0200

    Major update: - Added modules; - Reading data compleated; - Minor css fixes;

--- main.js
+++ main.js
@@ -15,9 +15,26 @@ var links = [
     }
 ];
 
+function read() {
+    links = [];
+    var sections = document.querySelectorAll('.section');
+    for (let i = 0; i < sections.length; i++) {
+        var section = {
+            name: sections[i].children[0].value,
+            list: {}
+        }
+        var linksList = sections[i].children[3].querySelectorAll('li');
+        for (let i = 0; i < linksList.length; i++) {
+            var title = linksList[i].children[1].children[0].value;
+            var link = linksList[i].children[1].children[1].value;
+            section.list[title] = link;
+        }
+        links.push(section);
+    }
+    console.log(links);
+}
 
 function initTables() {
-
     function generateLinks(object) {
         var linksArray = '';
         for (var i = 0; i < Object.keys(object).length; i++) {
@@ -26,10 +43,10 @@ function initTables() {
                                     <a href="${object[Object.keys(object)[i]]}"></a>
                                 </span> 
                                 <span class="row">
-                                    <input class="row-title" value="${Object.keys(object)[i]}">  
-                                    <input class="row-link" value="${object[Object.keys(object)[i]]}" onkeyup="changeUrl(this)"> 
+                                    <input class="row-title" value="${Object.keys(object)[i]}" onkeyup="read()">  
+                                    <input class="row-link" value="${object[Object.keys(object)[i]]}" onkeyup="row.changeUrl(this)"> 
                                 </span> 
-                                <span class="delete-row" onclick="deleteRow(this)">-</span> 
+                                <span class="delete-row" onclick="row.remove(this.parentElement)">-</span> 
                             </li>`;
         }
         return linksArray;
@@ -39,7 +56,8 @@ function initTables() {
         var additionalHTML = `
             <div class="section">
                 <input class="section-tops" value="${links[i].name}">
-                <div class="section-tops" onclick="addEmptyRow(this)">+</div>
+                <div class="section-tops" onclick="row.add(this.parentElement, 'test', 'http://')">+</div>
+                <div class="section-tops" onclick="section.remove(this.parentElement)">-</div>
                 <ul class="sortable connectedSortable">
                     ${generateLinks(links[i].list)}
                 </ul>
@@ -52,47 +70,81 @@ function initTables() {
 function initDrag() {
     $(".sortable").sortable({
         connectWith: ".connectedSortable",
-        placeholder: "ui-state-highlight"
+        placeholder: "ui-state-highlight",
+        cursor: "move",
+        delay: 150,
+        opacity: 0.5,
+        update: function (e) {
+            read();
+        }
     }).disableSelection();
 }
 
-function addEmptyRow(object) {
-    addRow(object.parentElement, 'test', 'http://');
-}
-
-function addRow(section, name, url) {
-    var rowHTML = `<li class="ui-state-default"> 
+var row = function () {
+    function add(section, name, url) {
+        var rowHTML = `<li class="ui-state-default"> 
                         <span class="ui-icon ui-icon-arrowthick-2-n-s">
                             <a href="${url}"></a>
                         </span> 
                         <span class="row">
                             <input class="row-title" value="${name}">  
-                            <input class="row-link" value="${url}" onkeyup="changeUrl(this)"> 
+                            <input class="row-link" value="${url}" onkeyup="row.changeUrl(this)"> 
                         </span> 
-                        <span class="delete-row" onclick="deleteRow(this)">-</span> 
+                        <span class="delete-row" onclick="row.remove(this.parentElement)">-</span> 
                     </li>`;
-    section.children[2].innerHTML += rowHTML;
-}
+        section.children[3].innerHTML += rowHTML;
+        read();
+    }
 
-function deleteRow(object) {
-    $(object).parent().remove();
-}
+    function remove(object) {
+        var msg = confirm("Remove this link?");
+        if (msg === true) {
+            $(object).remove();
+            read();
+        }
 
-function addSection() {
-    document.body.innerHTML += `
+    }
+
+    function changeUrl(object) {
+        object.parentElement.parentElement.children[0].children[0].href = object.value;
+        read();
+    }
+
+    return {
+        add,
+        remove,
+        changeUrl
+    }
+}();
+
+var section = function () {
+    function add() {
+        document.body.innerHTML += `
             <div class="section">
                 <input class="section-tops" value="New Section">
-                <div class="section-tops" onclick="addEmptyRow(this)">+</div>
+                <div class="section-tops" onclick="row.add(this.parentElement, 'test', 'http://')">+</div>
+                <div class="section-tops" onclick="section.remove(this.parentElement)">-</div>
                 <ul class="sortable connectedSortable">
                 </ul>
             </div>
         `;
-    initDrag();
-}
+        initDrag();
+        read();
+    }
 
-function changeUrl(object) {
-    object.parentElement.parentElement.children[0].children[0].href = object.value;
-}
+    function remove(object) {
+        var msg = confirm("Remove this section?");
+        if (msg === true) {
+            $(object).remove();
+            read();
+        }
+    }
+
+    return {
+        add,
+        remove
+    }
+}();
 
 window.addEventListener("load", function () {
     initTables();
